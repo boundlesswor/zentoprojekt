df3fe72602f60bd03030a90886fd59d659bf1637
boundlesswor <workboundless@gmail.com>
2025-08-16 18:30:08 +0300

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,8 @@
 "use client";
+
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const revalidate = false;
+export const fetchCache = "force-no-store";
 
 import { useState, useEffect } from "react"
 import { useRouter } from "next/navigation"
